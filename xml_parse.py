4d3941f984210d66cb3f1bcaf9ae4d7d7390a762
Owen Seltzer <seltzer.o@northeastern.edu>
2021-09-28 23:11:14 -0400

    Finished xml parsing

--- xml_parse.py
+++ xml_parse.py
@@ -1,24 +1,115 @@
 import xml.etree.ElementTree as ET
+import sys
+import itertools
+from dfa import dfa
+from transition import transition
+from runRead import run
+
+input = sys.stdin.read()
+testTree = ET.parse(input)
+tree = ET.parse(input)
+
+test = testTree.getroot()
+
+states = set()
+start = 'NOTFOUND'
+accept = set()
+alphabet = set()
+allTransitions = set()
+fromTransitions = []
+toTransitions = []
+readTransitions = []
+
+idToState = {}
+
+namesFinal = ""
+startFinal = ""
+finalFinal = ""
+
+if test.tag != 'automaton':
+    root = tree.getroot().find('automaton')
+else:
+    root = tree.getroot()
+
+for state in root.findall('state'):
+    idToState.update({state.get('id') : state.get('name')})
+    states.add(state.get('name'))
+    for subtag in state.findall('initial'):
+        if start == 'NOTFOUND':
+            start = state.get('name')
+    for subtag2 in state.findall('final'):
+        accept.add(state.get('name'))
+for transitionObj in root.findall('transition'):
+    fromVar = 0
+    toVar = 0
+    readVar = 0
+    for subtag3 in transitionObj.findall('from'):
+        #fromTransitions.append(subtag3.text)
+        fromVar = subtag3.text
+    for subtag4 in transitionObj.findall('to'):
+        #toTransitions.append(subtag4.text)
+        toVar = subtag4.text
+    for subtag5 in transitionObj.findall('read'):
+        #readTransitions.append(subtag5.text)
+        readVar = subtag5.text
+        alphabet.add(subtag5.text)
+    allTransitions.add(transition(idToState.get(fromVar), readVar, idToState.get(toVar)))
 
 
-# parses the given xml file that gives an automata
+for name in states:
+    namesFinal += name
+    namesFinal += " "
 
-def main():
-    try:
-        file = input()
-    except EOFError:
-        text = ""
-    automata = ET.parse(file)
-    root = automata.getroot()
-    for state in root.iter('state'):
-        print(state.get('name') + " ", end='')
-    print()
-    for state in root.iter('state'):
-        if state.find('initial') != None:
-            print(state.get('name') + " ")
-    for state in root.iter('state'):
-        if state.find('final') != None:
-            print(state.get('name') + " ")
+for st in start:
+    startFinal += st
+    startFinal += " "
 
-if __name__ == "__main__":
-    main()
+for fi in accept:
+    finalFinal += fi
+    finalFinal += " "
+
+dfa_object = dfa(states, alphabet, allTransitions, start, accept)
+
+one = itertools.product(alphabet, repeat = 1)
+two = itertools.product(alphabet, repeat = 2)
+three = itertools.product(alphabet, repeat = 3)
+four = itertools.product(alphabet, repeat = 4)
+five = itertools.product(alphabet, repeat = 5)
+
+total = []
+totalFinal = []
+
+totalFinal.append("")
+
+for m in one:
+    total.append(m)
+
+for m in two:
+    total.append(m)
+
+for m in three:
+    total.append(m)
+
+for m in four:
+    total.append(m)
+
+for m in five:
+    total.append(m)
+
+
+for i in total:
+    k = ""
+    for j in i:
+        k = k + j
+    totalFinal.append(k)
+
+totalFinalFinal = []
+
+for ji in totalFinal:
+    if run(ji, dfa_object):
+        totalFinalFinal.append(ji)
+
+for k in totalFinalFinal:
+    print(k)
+
+
